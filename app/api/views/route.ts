0d8d5d60367cad01c6b4d48575f39395ca4ff5f7
puru12 <purushottam.makura@gmail.com>
2024-02-02 22:30:39 +0545

    fix:views_api

--- app/api/views/route.ts
+++ app/api/views/route.ts
@@ -4,13 +4,30 @@ import { NextResponse } from "next/server";
 export async function POST(req:any){
     try {
         const body=await req.json();
-        const {views,blog_id}=body;
-        await prismadb.views.create({
-            data:{
-               blog_id,
-               views
+        const {blog_id}=body;
+        const findBlog=await prismadb.views.findFirst({
+            where:{
+                blog_id
             }
-        })
+        });
+
+        if(!findBlog){
+            await prismadb.views.create({
+                data:{
+                    blog_id,
+                    views:1
+                }
+            })
+        }else{
+            await prismadb.views.update({
+                where:{
+                    id:findBlog.id
+                },
+                data:{
+                    views:{increment:1}
+                }
+            })
+        }
        
         return NextResponse.json({success:"Successfully added views"},{status: 200});
         
