43450bae68537cbc0c23e49d8f7a78a8839f9314
puru12 <purushottam.makura@gmail.com>
2024-02-02 12:08:10 +0545

    fix:comment_api

--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req:any){
     try {
-        const body=await req.body;
+        const body=await req.json();
         const {fullName,email,comment,id}=body;
         await prismadb.comment.create({
             data:{
